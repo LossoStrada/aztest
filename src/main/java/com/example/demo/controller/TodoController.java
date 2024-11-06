93d54765c0016162a8f2211b6c736c10f954ca41
kimjunyeob <junyeob.kim@kt.com>
2024-11-06 18:08:51 +0900

    Restful API 개발

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TodoController {
@@ -9,5 +12,10 @@ public class TodoController {
     @GetMapping("/")
     public String home() {
         return "todoList";
+    }
+
+    @GetMapping(value = "/api/v1/user")
+    public ResponseEntity<?> returnId() {
+        return new ResponseEntity<>("82265634", HttpStatus.OK);
     }
 }
